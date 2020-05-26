b62ff07a55f86cbece29f081a3130592c8fd7874
IGavrichenko <gavr.imba@mail.ru>
2020-05-26 20:29:48 +0300

    jwt-auth strategy

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, UseGuards, Post, Request } from '@nestjs/common';
+import { Controller, UseGuards, Post, Request, Get } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
+    console.log('user', req.user);
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
---
+++ src/modules/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: 'secretKey', // TODO: need to moving it to .env file
+};
